611e7f3779ff4c46ceb301231e27d9b49c6b261c
Faby <faby@gervan.com.br>
2018-08-11 15:19:02 -0300

    Adicionado Json em todos os projetos e as referencias no Views

--- Conteudo/Conteudo/Program.cs
+++ Conteudo/Conteudo/Program.cs
@@ -16,7 +16,7 @@ namespace Conteudo
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Categorias());
+            Application.Run(new Conteudos());
         }
     }
 }
