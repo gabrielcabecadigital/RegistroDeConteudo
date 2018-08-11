4bc888fac6bf11ebb99e31b5111395492b8306a3
Gabriel Alencar <cavalomorreu@gmail.com>
2018-08-11 11:17:35 -0300

    Criei os models para categorias e conteudos

---
+++ Conteudo/Models/Dao/CategoriaDao.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Dao
+{
+    public class CategoriaDao
+    {
+        public static Categoria Get(int id)
+        {
+            return null;
+        }
+
+        public static List<Categoria> List()
+        {
+            return null;
+        }
+
+        public static bool Save(Categoria item)
+        {
+            return false;
+        }
+
+        public static bool Update(Categoria item)
+        {
+            return false;
+        }
+
+        public static bool Delete(Categoria item)
+        {
+            return false;
+        }
+    }
+}
---
+++ Conteudo/Models/Dao/ConteudoDao.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Dao
+{
+    public class ConteudoDao
+    {
+        public static Conteudo Get(int id)
+        {
+            return null;
+        }
+
+        public static List<Conteudo> List()
+        {
+            return null;
+        }
+
+        public static bool Save(Conteudo item)
+        {
+            return false;
+        }
+
+        public static bool Update(Conteudo item)
+        {
+            return false;
+        }
+
+        public static bool Delete(Conteudo item)
+        {
+            return false;
+        }
+    }
+}
